7a1b6100c0601bb9493fd98971aef05b8762c358
dianaimao <2284262314@qq.com>
2020-10-05 16:57:46 +0800

    添加项目文件。

---
+++ WpfApp1/App.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using Application = System.Windows.Application;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        private static NotifyIcon trayIcon;
+
+        private void ApplicationStartup(object sender, StartupEventArgs e)
+        {
+            RemoveTrayIcon();
+            AddTrayIcon();
+        }
+
+        private void AddTrayIcon()
+        {
+            string str1 = System.Environment.CurrentDirectory;
+
+            if (trayIcon != null)
+            {
+                return;
+            }
+            trayIcon = new NotifyIcon
+            {
+                   
+
+            //Icon = new System.Drawing.Icon("C:/Users/Administrator/Downloads/01.ico"),
+
+           Icon = new Icon(
+   System.Windows.Application.GetResourceStream(
+     new Uri(
+     "01.ico", UriKind.Relative)
+   ).Stream
+ ),
+
+            Text = "NotifyIconStd"
+            };
+            trayIcon.Visible = true;
+
+            ContextMenu menu = new ContextMenu();
+
+            MenuItem closeItem = new MenuItem();
+            closeItem.Text = "Close";
+            closeItem.Click += new EventHandler(delegate { this.Shutdown(); });
+
+            MenuItem addItem = new MenuItem();
+            addItem.Text = "Menu";
+
+            menu.MenuItems.Add(addItem);
+            menu.MenuItems.Add(closeItem);
+
+            trayIcon.ContextMenu = menu;    //设置NotifyIcon的右键弹出菜单
+        }
+
+        private void RemoveTrayIcon()
+        {
+            if (trayIcon != null)
+            {
+                trayIcon.Visible = false;
+                trayIcon.Dispose();
+                trayIcon = null;
+            }
+        }
+
+        private void ApplicationExit(object sender, ExitEventArgs e)
+        {
+            RemoveTrayIcon();
+        }
+    }
+
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Windows.UI.Notifications;
+using Windows.Data.Xml.Dom;
+using Microsoft.Toolkit.Uwp.Notifications;
+using Windows.Storage;
+using System.Windows.Forms; // NotifyIcon control
+using System.Drawing; // Icon
+
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NotifyIcon notifyIcon;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+        }
+        void click(object sender, RoutedEventArgs e)
+        {
+            string str1 = System.Environment.CurrentDirectory;
+            // Configure and show a notification icon in the system tray
+            this.notifyIcon = new NotifyIcon();
+            this.notifyIcon.BalloonTipText = "Hello, NotifyIcon!";
+            this.notifyIcon.Text = "Hello, NotifyIcon!";
+            //this.notifyIcon.Icon = new System.Drawing.Icon("‪C:/Users/Administrator/Downloads/01.ico");
+            this.notifyIcon.Icon = new Icon(
+   System.Windows.Application.GetResourceStream(
+     new Uri(
+     "01.ico", UriKind.Relative)
+   ).Stream
+ );
+
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.ShowBalloonTip(1000);
+        }
+
+        // 弹出 toast 通知（图片来自 http，image 的 placement 为 appLogoOverride，image 的 addImageQuery 为 true）
+        private void ButtonShowToast_Click(object sender, RoutedEventArgs e)
+        {
+            string title = "featured picture of the day";
+            string content = "beautiful scenery";
+            //string image = "https://picsum.photos/360/180?image=104";
+            string logo = "https://picsum.photos/64?image=883";
+            //本地 格式png 大小 
+            string image = "Images/js.png";
+            string xmlString =
+            $@"<toast><visual>
+       <binding template='ToastGeneric'>
+       <text>{title}</text>
+       <text>{content}</text>
+       <image src='{image}'/>
+       <image src='{logo}' placement='appLogoOverride' hint-crop='circle'/>
+       </binding>
+      </visual></toast>";
+
+            XmlDocument toastXml = new XmlDocument();
+            toastXml.LoadXml(xmlString);
+
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+
+
+}
